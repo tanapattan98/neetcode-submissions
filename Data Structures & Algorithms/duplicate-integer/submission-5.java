a228acb2499ef4912d4be5eb078d010396d24469
tanapattan98 <tanapatfrance120698@gmail.com>
2026-05-07 01:01:57 +0100

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet(nums.length);
+
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            } 
+        }
+        return false;
+    }
+}
