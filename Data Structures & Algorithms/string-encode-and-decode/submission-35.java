8cf5cb3a44bfd5d1161acec3fd98c84d861cd83f
tanapattan98 <tanapatfrance120698@gmail.com>
2026-05-10 05:39:29 +0100

    Add: string-encode-and-decode - submission-35

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-35.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    private static final char DELIM = '*';
+
+    public String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+
+        for (String str : strs) {
+            encodedString.append(str.length()).append(DELIM).append(str);
+        }
+        return encodedString.toString();
+    }
+
+    public List<String> decode(String str) {
+        int i = 0;
+        ArrayList<String> decodedStrings = new ArrayList<>();
+        
+        while(i < str.length()) {
+            int j = i;
+            while(str.charAt(j) != DELIM) {
+                j++;
+            }
+            
+            int length= Integer.parseInt(str.substring(i, j));
+            j++;
+ 
+            String decodedString = str.substring(j, j + length);
+            decodedStrings.add(decodedString);
+
+            
+            i = j + length;
+        }
+
+        return decodedStrings;
+    }
+}
