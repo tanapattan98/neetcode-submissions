9757e28b4d5d3e2afd4dbc10d93cdcd80ebc0923
tanapattan98 <tanapatfrance120698@gmail.com>
2026-05-25 01:16:43 +0100

    Add: depth-of-binary-tree - submission-10

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-10.ts
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+
+    // // Recursive DFS
+    // maxDepth(root: TreeNode | null): number {
+
+    //     if (root == null) return 0;
+
+    //     return 1 + Math.max(this.maxDepth(root.left), this.maxDepth(root.right));
+
+    // }
+
+    // // Iterative BFS
+    // maxDepth(root: TreeNode | null): number {
+
+    //     if (root == null) return 0;
+
+    //     const queue: TreeNode[] = [root];
+    //     let depth = 0
+
+    //     while(queue.length > 0) {
+    //         const levelSize = queue.length;
+
+    //         for (let i = 0; i < levelSize; i++) {
+    //             const node = queue.shift();
+    //             if (node.left) queue.push(node.left);
+    //             if (node.right) queue.push(node.right);
+    //         }
+
+    //         depth++;
+    //     }
+    //     return depth;
+    // }
+
+    // Iterative dfs
+    maxDepth(root: TreeNode | null): number {
+
+        if (root === null) return 0;
+
+        const stack: [TreeNode, number][] = [[root, 1]];
+        let depth = 0;
+
+        while (stack.length > 0) {
+            const [node, currentDepth] = stack.pop();
+            depth = Math.max(depth, currentDepth);
+
+            if (node.left) stack.push([node.left, currentDepth + 1]);
+            if (node.right) stack.push([node.right, currentDepth + 1]);
+        }
+
+        return depth;
+    }
+}
