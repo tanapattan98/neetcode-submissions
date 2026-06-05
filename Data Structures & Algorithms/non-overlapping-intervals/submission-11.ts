fffd84049f3d57f21a813bbbabe9d8a65efeff03
tanapattan98 <tanapatfrance120698@gmail.com>
2026-06-05 15:31:03 +0100

    Add: non-overlapping-intervals - submission-11

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-11.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals: number[][]): number {
+        intervals.sort((a, b) => a[1] - b[1]);
+
+        let prevEnd = intervals[0][1];
+        let result = 0;
+
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < prevEnd) {
+                result++;
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return result;
+    }
+}
