c727c07d72ae202dc90c02e219e3d0411bd075ff
tanapattan98 <tanapatfrance120698@gmail.com>
2026-06-05 12:53:09 +0100

    Add: insert-new-interval - submission-4

---
+++ Data Structures & Algorithms/insert-new-interval/submission-4.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @param {number[]} newInterval
+     * @return {number[][]}
+     */
+    insert(intervals: number[][], newInterval: number[]): number[][] {
+        const result: number [][] = [];
+
+        for (let i = 0; i < intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                result.push(newInterval);
+                result.push(...intervals.slice(i));
+                return result;
+            } else if (newInterval[0] > intervals[i][1]) {
+                result.push(intervals[i]);
+            } else {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+        }
+
+        result.push(newInterval);
+        return result;
+    }
+}
